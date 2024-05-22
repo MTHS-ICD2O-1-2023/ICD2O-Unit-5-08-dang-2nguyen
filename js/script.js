8052150952cbacc69d4256e543b729267f0f105a
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-05-22 00:24:04 +0000

    sdvsvsdvs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Dang All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dang
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+function checking() {
+  const divisor = parseInt(document.getElementById("divisor").value)
+  const divided = parseInt(document.getElementById("divided").value)
+  let counter = 0
+  let answer = divisor
+  let steps = divisor + ""
+  while (true) {
+
+    answer -= divided
+    steps += "-" + divided + "=" + answer + ''
+    counter++
+
+    if(answer < divided) {
+      break
+    }
+  }
+  document.getElementById("result").innerHTML = steps
+  document.getElementById("result2").innerHTML = "The answer is " + counter + " remain: " + answer
+
+  
+
+}
